40e7a5829063236ca4f63bd0332355a35684a748
Cesar Oliva <cesaroliva.work@gmail.com>
2026-05-28 17:52:54 -0600

    Adding auth protection at dates.ts

--- convex/dates.ts
+++ convex/dates.ts
@@ -6,12 +6,20 @@ export const getMoodsByMonth = query({
         year: v.number(),
         month: v.number()
     }, handler: async (ctx, args) => {
+       const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Not authenticated")
+        }
+
+        const userId = identity.subject;
+
         const start = `${args.year}-${String(args.month + 1).padStart(2, "0")}-01`;
         const end = `${args.year}-${String(args.month + 1).padStart(2, "0")}-31`;
 
         const moods =  await ctx.db.query("dates")
             .withIndex("by_user_date", (q)=>
-                q.eq("userId", 'userId') //userId
+                q.eq("userId", userId) //userId
                 .gte("date", start)
                 .lte("date", end)
             )
@@ -28,8 +36,16 @@ export const registerMood = mutation({
         day: v.number(),
         mood: v.string()
     }, handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Not authenticated")
+        }
+
+        const userId = identity.subject;
+
         const mood = await ctx.db.insert("dates", {
-            userId: 'userId',
+            userId: userId,
             date: `${args.year}-${String(args.month).padStart(2, "0")}-${String(args.day).padStart(2, "0")}`,
             mood: args.mood,
         });
@@ -44,9 +60,17 @@ export const deleteMood = mutation({
         month: v.number(),
         day: v.number(),
     }, handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Not authenticated")
+        }
+
+        const userId = identity.subject;
+
         const dateId = await ctx.db.query("dates")
             .withIndex("by_user_date", (q)=>
-                q.eq("userId", 'userId')
+                q.eq("userId", userId)
                 .eq("date", `${args.year}-${String(args.month).padStart(2, "0")}-${String(args.day).padStart(2, "0")}`)
             )
             .first();
